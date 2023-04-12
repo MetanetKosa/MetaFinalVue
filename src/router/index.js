3678eae3ff9da293d6d52e12037e243af06041ec
seeun jeong <sesej4512@gmail.com>
2023-04-12 15:12:31 +0900

    Feat: add Admin page, Admin bootstrap, move bootstrap route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../components/pages/Home.vue';
 import Login from '../components/pages/Login.vue';
+import Admin from '../components/pages/Admin.vue';
 
 
 const router = createRouter({
@@ -16,7 +17,13 @@ const router = createRouter({
             path: '/login',
             name: 'Login',
             component: Login
-        }
+        },
+
+        {
+            path: '/admin',
+            name: 'Admin',
+            component: Admin
+        },
     ]
 });
 
