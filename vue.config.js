5fdd071d3c0067e1b1c407e332c1e42ef20671fe
seeun jeong <sesej4512@gmail.com>
2023-04-12 10:42:33 +0900

    Feat: add Header component

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // Component name "컴포넌트명" should always be multi-word  vue/multi-word-component-names 에러 수정
+  lintOnSave : false
 })
