413cc751161da8526b5b2cc0066ff5af67097df3
seeun jeong <sesej4512@gmail.com>
2023-04-13 10:07:13 +0900

    Fix: Error fix test2

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,11 @@
 
 <script>
 import Header from '@/components/common/Header.vue'
-import Footer from '@/components/common/Footer.vue'
 
 export default {  
   name: 'App',
   components: {
-    Header,
-    Footer
+    Header
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../components/pages/Home.vue';
-<<<<<<< HEAD
 import Login from '../components/pages/member/Login.vue';
-import Signup from '../components/pages/member/Signup.vue';
-import Admin from '../components/pages/admin/Admin.vue';
-=======
-import Login from '../components/pages/Login.vue';
 import AdminRegister from '../components/pages/admin/AdminRegister.vue';
->>>>>>> 219f64ef4bdb9e707e15d36d22d6f4efca4cd5fd
 
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: [{
+    routes: [
+        {
             path: '/',
             name: 'Home',
             component: Home
@@ -22,25 +17,11 @@ const router = createRouter({
             name: 'Login',
             component: Login
         },
-<<<<<<< HEAD
-        {
-            path: '/signup',
-            name: 'Signup',
-            component: Signup
-        },
-        {
-            path: '/admin',
-            name: 'Admin',
-            component: Admin
-        }
-       
-=======
         {
             path: '/adminregister',
             name: 'AdminRegister',
             component: AdminRegister
         }
->>>>>>> 219f64ef4bdb9e707e15d36d22d6f4efca4cd5fd
     ]
 });
 
