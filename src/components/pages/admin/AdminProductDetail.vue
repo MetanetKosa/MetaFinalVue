b343262902f20a0fbb81151ec80e4c81d1b1e6cf
didwndbs <didwndbs@naver.com>
2023-04-14 17:45:27 +0900

    Feat: add produtDetail

--- src/components/pages/admin/AdminProductDetail.vue
+++ src/components/pages/admin/AdminProductDetail.vue
@@ -39,7 +39,7 @@
                     <h3 class="my-3">LOWA Men’s Renegade GTX Mid Hiking Boots Review</h3>
                       <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terr.</p>
                       <hr>
-                    <h4>Available Colors</h4>
+                    <h4>색상</h4>
                     <div class="btn-group btn-group-toggle" data-toggle="buttons">
                       <label class="btn btn-default text-center active">
                         <input type="radio" name="color_option" id="color_option_a1" autocomplete="off" checked>
@@ -47,33 +47,9 @@
                         <br>
                         <i class="fas fa-circle fa-2x text-green"></i>
                       </label>
-                      <label class="btn btn-default text-center">
-                        <input type="radio" name="color_option" id="color_option_a2" autocomplete="off">
-                          Blue
-                         <br>
-                         <i class="fas fa-circle fa-2x text-blue"></i>
-                       </label>
-                        <label class="btn btn-default text-center">
-                         <input type="radio" name="color_option" id="color_option_a3" autocomplete="off">
-                         Purple
-                         <br>
-                          <i class="fas fa-circle fa-2x text-purple"></i>
-                </label>
-                <label class="btn btn-default text-center">
-                  <input type="radio" name="color_option" id="color_option_a4" autocomplete="off">
-                  Red
-                  <br>
-                  <i class="fas fa-circle fa-2x text-red"></i>
-                </label>
-                <label class="btn btn-default text-center">
-                  <input type="radio" name="color_option" id="color_option_a5" autocomplete="off">
-                  Orange
-                  <br>
-                  <i class="fas fa-circle fa-2x text-orange"></i>
-                </label>
               </div>
 
-              <h4 class="mt-3">Size <small>Please select one</small></h4>
+              <!-- <h4 class="mt-3">Size <small>Please select one</small></h4>
               <div class="btn-group btn-group-toggle" data-toggle="buttons">
                 <label class="btn btn-default text-center">
                   <input type="radio" name="color_option" id="color_option_b1" autocomplete="off">
@@ -81,62 +57,26 @@
                   <br>
                   Small
                 </label>
-                <label class="btn btn-default text-center">
-                  <input type="radio" name="color_option" id="color_option_b2" autocomplete="off">
-                  <span class="text-xl">M</span>
-                  <br>
-                  Medium
-                </label>
-                <label class="btn btn-default text-center">
-                  <input type="radio" name="color_option" id="color_option_b3" autocomplete="off">
-                  <span class="text-xl">L</span>
-                  <br>
-                  Large
-                </label>
-                <label class="btn btn-default text-center">
-                  <input type="radio" name="color_option" id="color_option_b4" autocomplete="off">
-                  <span class="text-xl">XL</span>
-                  <br>
-                  Xtra-Large
-                </label>
-              </div>
+              </div> -->
 
               <div class="bg-gray py-2 px-3 mt-4">
                 <h2 class="mb-0">
-                  $80.00
+                  구매가격
                 </h2>
                 <h4 class="mt-0">
-                  <small>Ex Tax: $80.00 </small>
+                  <small>월렌탈료: $80.00 </small>
                 </h4>
               </div>
 
               <div class="mt-4">
                 <div class="btn btn-primary btn-lg btn-flat">
-                  <i class="fas fa-cart-plus fa-lg mr-2"></i>
-                  Add to Cart
+                  수정
                 </div>
 
                 <div class="btn btn-default btn-lg btn-flat">
-                  <i class="fas fa-heart fa-lg mr-2"></i>
-                  Add to Wishlist
+                  삭제
                 </div>
               </div>
-
-              <div class="mt-4 product-share">
-                <a href="#" class="text-gray">
-                  <i class="fab fa-facebook-square fa-2x"></i>
-                </a>
-                <a href="#" class="text-gray">
-                  <i class="fab fa-twitter-square fa-2x"></i>
-                </a>
-                <a href="#" class="text-gray">
-                  <i class="fas fa-envelope-square fa-2x"></i>
-                </a>
-                <a href="#" class="text-gray">
-                  <i class="fas fa-rss-square fa-2x"></i>
-                </a>
-              </div>
-
             </div>
           </div>
           <div class="row mt-4">
