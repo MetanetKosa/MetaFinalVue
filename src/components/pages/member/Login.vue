298c34bf8392f6184a23864ea5059e2f7c3c0bff
eunyoung56 <eunyeong1328@daum.net>
2023-04-14 17:15:30 +0900

    Feat:add login under implementation

--- src/components/pages/member/Login.vue
+++ src/components/pages/member/Login.vue
@@ -5,21 +5,21 @@
             <div class="col-md-8 col-lg-6 col-xl-5 d-flex align-items-center">
                 <div class="w-100 py-5 px-md-5 px-xxl-6 position-relative">
                     <div class="mb-5"><img class="img-fluid mb-3" src="img/logo-square.svg" alt="..." style="max-width: 4rem;">
-                        <h2>Welcome back</h2>
+                        <h2>로그인</h2>
                     </div>
-                    <form class="form-validate">
+                    <form @submit.prevent="login" class="form-validate">
                         <div class="mb-4">
-                            <label class="form-label" for="loginUsername"> Email Address</label>
-                            <input class="form-control" name="loginUsername" id="loginUsername" type="email" placeholder="name@address.com" autocomplete="off" required data-msg="Please enter your email">
+                            <label class="form-label" for="memId"> 아이디</label>
+                            <input class="form-control" v-model="state.form.memId" name="memId" id="memId" type="text" placeholder="아이디를 입력해주세요" autocomplete="off" required data-msg="Please enter your email">
                         </div>
                         <div class="mb-4">
                             <div class="row">
                                 <div class="col">
-                                    <label class="form-label" for="loginPassword"> Password</label>
+                                    <label class="form-label" for="memPw"> 비밀번호</label>
                                 </div>
                                 <div class="col-auto"><a class="form-text small text-primary" href="#">Forgot password?</a></div>
                             </div>
-                            <input class="form-control" name="loginPassword" id="loginPassword" placeholder="Password" type="password" required data-msg="Please enter your password">
+                            <input class="form-control" v-model="state.form.memPw" name="memPw" id="memPw" placeholder="비밀번호를 입력해주세요" type="password" required data-msg="Please enter your password">
                         </div>
                         <div class="mb-4">
                             <div class="form-check">
@@ -29,7 +29,7 @@
                         </div>
                         <!-- Submit-->
                         <div class="d-grid">
-                            <button class="btn btn-lg btn-primary">Sign in</button>
+                            <button class="btn btn-lg btn-primary">로그인</button>
                         </div>
                         <hr class="my-3 hr-text letter-spacing-2" data-content="OR">
                         <div class="d-grid gap-2">
@@ -55,7 +55,37 @@
 </template>
 
 <script>
+import axios from 'axios';
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+
 export default {
+    setup(){
+        const router = useRouter();
+        const state = reactive({
+            login: [],
+            form: {
+                memId: "",
+                memPw: ""
+            },
+        })
+
+        const login = () => {
+            console.log("memId확인" + state.form.memId);
+            console.log("memPw확인" + state.form.memPw);
+
+            const args = JSON.parse(JSON.stringify(state.form));
+            axios.post("/auth/login", args).then(() => {
+                alert("로그인");
+                router.push({path: "/login"})
+            })
+        }
+
+        return{
+            state,
+            login
+        }
+    }
 }
 </script>
 
