2e5acd04d24f78de72ef9fd0fa93cb260395e78f
didwndbs <didwndbs@naver.com>
2023-04-16 05:00:41 +0900

    Fix:modify add fileUpload

--- src/components/pages/admin/AdminRegister.vue
+++ src/components/pages/admin/AdminRegister.vue
@@ -137,16 +137,30 @@
                                 <div id="actions" class="row">
                                     <div class="col-lg-6">
                                         <div class="btn-group w-100">
-                                        <span class="btn btn-default col fileinput-button">
-                                            <span>대표 이미지 등록</span>
+                                            <label for="mainImageInput" ref="mainInput" class="btn btn-default col fileinput-button" >
+                                                대표 이미지 등록
+                                            </label>
+                                            <input id="mainImageInput" type="file" class="sr-only" @change="handleMainChange" >
+                                            <label for="addImageInput" class="btn btn-default col fileinput-button" multiple>
+                                                <span>추가 이미지 등록</span>
+                                            </label>
+                                            <input id="addImageInput" type="file" class="sr-only" @change="handleAddChange" multiple>
+                                            <label for="detailImageInput" class="btn btn-default col fileinput-button">
+                                                <span>상세 이미지 등록</span>
+                                            </label>
+                                            <input id="detailImageInput" type="file" class="sr-only" @change="handleDetailChange">
+                                        </div>
+                                        <!-- <div class="btn-group w-100">
+                                        <span class="btn btn-default col fileinput-button" @change="handleMainChange">
+                                            대표 이미지 등록
                                         </span>
-                                        <span class="btn btn-default col fileinput-button">
+                                        <span class="btn btn-default col fileinput-button" multiple @change="handleAddChange">
                                             <span>추가 이미지 등록</span>
                                         </span>
-                                         <span class="btn btn-default col fileinput-button">
+                                         <span class="btn btn-default col fileinput-button" @change="handleDetailChange">
                                             <span>상세 이미지 등록</span>
                                         </span>
-                                        </div>
+                                        </div> -->
                                     </div>
                                     <div class="col-lg-6 d-flex align-items-center">
                                         <div class="fileupload-process w-100">
@@ -155,7 +169,7 @@
                                     </div>
                                 </div>
                                 <div class="table table-striped files" id="previews">
-                                <div id="template" class="row mt-2">
+                                <!-- <div id="template" class="row mt-2">
                                     <div class="col-auto">
                                         <span class="preview"><img src="data:," alt="" data-dz-thumbnail /></span>
                                     </div>
@@ -173,7 +187,7 @@
                                             </button>
                                         </div>
                                     </div>
-                                </div>
+                                </div> -->
                                  </div>
                             </div>
                      
@@ -184,9 +198,10 @@
                             <div class="form-group">
                                 <div class="btn btn-default btn-file">
                                     <i class="fas fa-paperclip"></i> 설명서 등록
-                                    <input type="file" @change="handleFileChange">
+                                    <input type="file" @change="handleFileChange" accept="application/pdf">
                                 </div>
-                                 <p class="help-block">{{file.name}}</p>
+                                 <!-- <p class="help-block" v-for="file in state.files" :key="file.name">{{ file.name }}</p> -->
+                                    <p class="help-block">{{ state.form.product_guide }}</p>
                             </div>
                         </div>
                     </div>
@@ -208,46 +223,40 @@
 
 <script>
 import { ref, onMounted } from 'vue'
-import Dropzone from 'dropzone'
+// import Dropzone from 'dropzone'
 import axios from "axios";
 import Sidebar from '@/components/pages/admin/Sidebar.vue';
 import {reactive} from "vue";
 import {useRouter} from 'vue-router';
 
 export default {
-     name: 'MyDropzone',
+    //  name: 'MyDropzone',
     components: {
     Sidebar
   },
 
     setup(){
-        const dropzone = ref(null)
+    //     const dropzone = ref(null)
 
-    onMounted(() => {
-      Dropzone.autoDiscover = false
+    // onMounted(() => {
+    //   Dropzone.autoDiscover = false
 
-      const previewNode = document.querySelector('#template')
-      previewNode.id = ''
-      const previewTemplate = previewNode.parentNode.innerHTML
-      previewNode.parentNode.removeChild(previewNode)
+    //   const previewNode = document.querySelector('#template')
+    //   previewNode.id = ''
+    //   const previewTemplate = previewNode.parentNode.innerHTML
+    //   previewNode.parentNode.removeChild(previewNode)
 
-      dropzone.value = new Dropzone(document.body, {
-        url: '/target-url',
-        thumbnailWidth: 80,
-        thumbnailHeight: 80,
-        parallelUploads: 20,
-        previewTemplate: previewTemplate,
-        autoQueue: false,
-        previewsContainer: '#previews',
-        clickable: '.fileinput-button'
-      })
-
-    //   dropzone.value.on('addedfile', (file) => {
-    //     file.previewElement.querySelector('.start').onclick = function () {
-    //       dropzone.value.enqueueFile(file)
-    //     }
+    //   dropzone.value = new Dropzone(document.body, {
+    //     url: '/target-url',
+    //     thumbnailWidth: 80,
+    //     thumbnailHeight: 80,
+    //     parallelUploads: 20,
+    //     previewTemplate: previewTemplate,
+    //     autoQueue: false,
+    //     previewsContainer: '#previews',
+    //     clickable: '.fileinput-button'
     //   })
-    })
+    // })
       const router = useRouter();
       const state = reactive({
       products: [],
@@ -267,8 +276,11 @@ export default {
         product_model:"",
         product_rentalPrice: "",
         product_price : "",
-        product_color :""
+        product_color :"",
+        product_guide:"",
+        detail_url:"",
       },
+       files:[],
     })
     const submit = () => {
       const args = JSON.parse(JSON.stringify(state.form));
@@ -278,35 +290,152 @@ export default {
       })
     }
 
-    reactive({ 
-        files:[],
-    })
-
-    const handleFileChange=(e) => {
-    const formData = new FormData();
-    for(const file of e.target.files){
-        formData.append("files", file);
-        console.log(file);
-    }
-     axios.post("/upload/file", formData,{
-        //파일 업로드는 multipart/form-data 타입 사용 
-             headers: {'Content-Type': 'multipart/form-data'}
+    //이미지 업로드 처리
+    const handleImageUpload = (event, fileType) => {
+        const formData = new FormData();
+          if (event.target.files) {
+            if (fileType !== "files") {
+            const file = event.target.files[0];
+            formData.append("file", file);
+            }
+             else {
+            const files = event.target.files;
+                for (const file of files) {
+                    formData.append("files", file);
+                }
+            }
+          }
+        axios.post(`/upload/${fileType}`, formData, {
+            headers: {'Content-Type': 'multipart/form-data'}
         })
         .then(() => {
-        console.log("등록되었습니다");
+            console.log("이미지가 등록되었습니다.");
         })
         .catch(error => {
             console.log(error);
-            console.log("실패입니다");
+            console.log("이미지 업로드에 실패하였습니다.");
         });
- }
-  
-    
+    };
+
+        // 설명서 처리
+        const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        state.form.product_guide = file.name;
+        handleImageUpload(event, "guide");
+        };
+
+        // 대표 이미지 처리
+        const handleMainChange = (event) => {
+        const file = event.target.files[0];
+        state.form.img_url = file.name;
+        handleImageUpload(event, "file");
+        };
+
+        // 추가 이미지 처리
+        const handleAddChange = (event) => {
+        state.files = [...state.files, ...event.target.files];
+        handleImageUpload(event, "files");
+        };
+
+        // 상세 이미지 처리
+        const handleDetailChange = (event) => {
+        const file = event.target.files[0];
+        state.form.detail_url = file.name;
+        handleImageUpload(event, "file");
+        };
+
+//     //설명서 처리
+//     const handleFileChange=(e) => {
+//     const formData = new FormData();
+//         const file = e.target.files[0];
+//         formData.append("file", file);
+//         console.log(file);
+//         state.form.product_guide = file.name;
+//     //}
+//      axios.post("/upload/guide", formData,{
+//         //파일 업로드는 multipart/form-data 타입 사용 
+//              headers: {'Content-Type': 'multipart/form-data'}
+//         })
+//         .then(() => {
+//         console.log("등록되었습니다");
+//         })
+//         .catch(error => {
+//             console.log(error);
+//             console.log("실패입니다");
+//         });
+//     }
+
+//     //대표 이미지 처리
+//   const handleMainChange=(e) => {
+//         const formData = new FormData();
+//         const file = e.target.files[0];
+//         formData.append("file", file);
+//         console.log(file);
+//         state.form.img_url = file.name;
+
+//         axios.post("/upload/file", formData,{
+//         //파일 업로드는 multipart/form-data 타입 사용 
+//              headers: {'Content-Type': 'multipart/form-data'}
+//         })
+//         .then(() => {
+//         console.log("등록되었습니다");
+//         })
+//         .catch(error => {
+//             console.log(error);
+//             console.log("실패입니다");
+//         });
+//  }
+//  //추가 이미지 처리
+//    const handleAddChange=(e) => {
+//     const formData = new FormData();
+//         for(const file of e.target.files){
+//             formData.append("files", file);
+//              console.log(file);
+//             state.files.push(file)
+
+//         }
+       
+//         axios.post("/upload/files", formData,{
+//         //파일 업로드는 multipart/form-data 타입 사용 
+//              headers: {'Content-Type': 'multipart/form-data'}
+//         })
+//         .then(() => {
+//         console.log("등록되었습니다");
+//         })
+//         .catch(error => {
+//             console.log(error);
+//             console.log("실패입니다");
+//         });
+//  }
+
+//  //상세 이미지 처리
+//  const handleDetailChange=(e) => {
+//         const formData = new FormData();
+//         const file = e.target.files[0];
+//         formData.append("file", file);
+//         console.log(file);
+//         state.form.detail_url = file.name;
+
+//         axios.post("/upload/file", formData,{
+//         //파일 업로드는 multipart/form-data 타입 사용 
+//              headers: {'Content-Type': 'multipart/form-data'}
+//         })
+//         .then(() => {
+//         console.log("등록되었습니다");
+//         })
+//         .catch(error => {
+//             console.log(error);
+//             console.log("실패입니다");
+//         });
+//  }
+
     return {
         state, 
         submit,
         handleFileChange,
-        dropzone
+        handleMainChange,
+        handleAddChange,
+         handleDetailChange
         }
   
     }
